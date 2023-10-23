552f503d0fee5594277054f995564a4ffffc2476
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-10-23 13:58:17 +0100

    Added function that populates a random number

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,34 @@
 import { Inter } from "next/font/google";
+import { useState, useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <>Hello world</>;
+  const [showIcons, setShowIcons] = useState<number[]>([]);
+
+  useEffect(() => {
+    let icons: number[] = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+
+    const randomIcons = (): number[] => {
+      for (let i = icons.length - 1; i > 0; i--) {
+        // Generate a random index
+        const j = Math.floor(Math.random() * (i + 1));
+
+        // Swap elements
+        [icons[i], icons[j]] = [icons[j], icons[i]];
+      }
+      return icons;
+    };
+    setShowIcons(randomIcons());
+  }, []);
+
+  return (
+    <>
+      <div>
+        {showIcons.map((icon, index) => (
+          <div key={index}>{icon}</div>
+        ))}
+      </div>
+    </>
+  );
 }
