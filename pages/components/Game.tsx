889a3ffa953c414cbd6340dc1ea70f89c662b675
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-10-24 15:07:07 +0100

    De structured props

--- pages/components/Game.tsx
+++ pages/components/Game.tsx
@@ -1,12 +1,21 @@
 import { useState, useEffect } from "react";
 
-interface Props {
-  showIcons: number[];
-  shuffleIcons: () => void;
-}
-
-const Game: React.FC<Props> = ({ showIcons, shuffleIcons }) => {
+const Game: React.FC = () => {
   const [card, setCard] = useState(false);
+  const [showIcons, setShowIcons] = useState<number[]>([]);
+
+  const randomIcons = () => {
+    let icons: number[] = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+    for (let i = icons.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [icons[i], icons[j]] = [icons[j], icons[i]];
+    }
+    setShowIcons(icons);
+  };
+
+  useEffect(() => {
+    randomIcons();
+  }, []);
 
   const handleShowCard = () => {
     setCard(true);
@@ -28,7 +37,7 @@ const Game: React.FC<Props> = ({ showIcons, shuffleIcons }) => {
       </div>
       <button
         onClick={() => {
-          const newIcons = shuffleIcons();
+          randomIcons();
         }}
       >
         Randomise
